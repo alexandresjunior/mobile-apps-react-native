8ca739279a7ab7b5de371ac885644a2fbbc34408
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-12-07 11:35:04 -0300

    Remoção dos estilos; Uso do SafeAreaView

--- ecommerce-app-expo/App.js
+++ ecommerce-app-expo/App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
+      <StatusBar />
       <Text>Olá, mundo!</Text>
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
