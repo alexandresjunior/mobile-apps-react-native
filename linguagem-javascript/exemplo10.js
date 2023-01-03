6be91c70b8f5709ab11a393b26d086e1200f2590
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2023-01-03 11:04:06 -0300

    Importação do arquivo javascript

---
+++ linguagem-javascript/exemplo10.js
@@ -0,0 +1,14 @@
+let contador = 0;
+
+function contar() {
+    contador++;
+    document.querySelector('h1').innerHTML = contador;
+
+    if (contador % 10 === 0) {
+        alert(`Contador = ${contador}`)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').onclick = contar;
+});
