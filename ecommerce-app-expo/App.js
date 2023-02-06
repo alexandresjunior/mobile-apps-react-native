e935e908a0aa3f810f05f09ad2ae03c0da42b999
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2023-02-06 09:42:03 -0300

    Atualização do componente App.js

--- ecommerce-app-expo/App.js
+++ ecommerce-app-expo/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import StackRotas from "./src/rotas/StackRotas";
 import { NavigationContainer } from "@react-navigation/native";
-import { InfoProvider } from "./src/contexts/GlobalContext";
+import { CarrinhoDeComprasProvider } from "./src/contextos/GlobalContext";
 
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar />
-      <InfoProvider>
+      <CarrinhoDeComprasProvider>
         <StackRotas />
-      </InfoProvider>
+      </CarrinhoDeComprasProvider>
     </NavigationContainer>
   );
 }
