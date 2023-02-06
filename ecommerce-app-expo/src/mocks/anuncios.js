a6f3298754f6102b9d89b6262de097fdcf41e5ae
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2023-02-06 09:35:58 -0300

    Adição dos mocks para fins didáticos

---
+++ ecommerce-app-expo/src/mocks/anuncios.js
@@ -0,0 +1,17 @@
+export const anuncios = [
+    {
+        titulo: "Troque e Economize",
+        descricao: "Desfrute de uma ótima economia",
+        textoBotao: "Saiba Mais"
+    },
+    {
+        titulo: "Curso React JS",
+        descricao: "Segundas e Quartas",
+        textoBotao: "Matricule-se"
+    },
+    {
+        titulo: "Curso Java Experience",
+        descricao: "Depois do Carnaval",
+        textoBotao: "Matricule-se"
+    },
+]
---
+++ ecommerce-app-expo/src/mocks/produtos.js
@@ -0,0 +1,22 @@
+export const produtos = [
+    {
+        nome: "Camisa Longa Branca",
+        preco: 100,
+        review: 4.9
+    },
+    {
+        nome: "Camisa Longa Branca",
+        preco: 100,
+        review: 4.9
+    },
+    {
+        nome: "Camisa Longa Branca",
+        preco: 100,
+        review: 4.9
+    },
+    {
+        nome: "Camisa Longa Branca",
+        preco: 100,
+        review: 4.9
+    },
+]
